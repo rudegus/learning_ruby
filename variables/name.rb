780437fbc3af63ea374016e9615ab581fc600f62
Rude Gus <rudegus34@gmail.com>
2019-04-08 18:54:28 -0400

    Exercises

--- variables/name.rb
+++ variables/name.rb
@@ -1,2 +1,8 @@
-name = gets.chomp
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'What is your last name?'
+last_name = gets.chomp
+name = first_name + ' ' + last_name
 puts name + ' is a big fan of Seinfeld.'
+10.times do puts name
+end
