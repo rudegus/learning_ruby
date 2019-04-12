d6efb024bdf16d2ae7b2710dc9c08127c01ab988
EC2 Default User <ec2-user@ip-172-31-41-132.us-east-2.compute.internal>
2019-04-12 15:48:32 +0000

    new projects

---
+++ my_projects/project1.rb
@@ -0,0 +1,10 @@
+loop do
+puts "Sienfeld is better than friends: true or false"
+answer = gets.chomp 
+ if answer == "true"
+     puts "You're damn right!"
+     break
+ end
+ puts "try again"
+end 
+    
---
+++ my_projects/yada_yada.rb
@@ -0,0 +1,5 @@
+puts "Sienfeld is better than friends: true or false"
+answer = gets.chomp
+while answer != "true"
+puts "yada yada"
+end 
